0d63b9efa3c431b3447071ba19b305dea2aac229
Tetyana <myronova.tetyana@gmail.com>
2017-07-10 22:21:25 +0000

    Small changes

--- exercises.sql
+++ exercises.sql
@@ -106,7 +106,7 @@ CREATE TABLE rooms(
     typeOfRoom INT NOT NULL,
     capacity INT NOT NULL,
     numberOfWindows INT NOT NULL,
-    features INT NOT NULL
+    features INT NOT NULL);
     -- INDEX type_of_room(typeOfRoom),
     -- FOREIGN KEY (typeOfRoom)
     --     REFERENCES rooms(id)
@@ -115,13 +115,13 @@ CREATE TABLE rooms(
     -- FOREIGN KEY (features)
     --     REFERENCES room_features(id)
     --     ON DELETE CASCADE    
-);
+-- );
 
 -----------------------------check if Foreign keys were created    
-SELECT * FROM information_schema.TABLE_CONSTRAINTS 
-WHERE information_schema.TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY' 
-AND information_schema.TABLE_CONSTRAINTS.TABLE_SCHEMA = 'hotel'
-AND information_schema.TABLE_CONSTRAINTS.TABLE_NAME = 'rooms';
+-- SELECT * FROM information_schema.TABLE_CONSTRAINTS 
+-- WHERE information_schema.TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY' 
+-- AND information_schema.TABLE_CONSTRAINTS.TABLE_SCHEMA = 'hotel'
+-- AND information_schema.TABLE_CONSTRAINTS.TABLE_NAME = 'rooms';
 
 -----------------------------Insert values into the table
 INSERT INTO rooms
@@ -159,15 +159,97 @@ VALUES
     ('janitor'), ('closets'), ('public'), ('laundry room'), ('gym'), ('regular'), ('penthouse'), ('vip');
     
 -----------------------------The list of Rooms available for rent on a specific date
-select date = '2017-07-10 00:00:00'
-SELECT * 
+SELECT *
 FROM rooms 
-WHERE date > 
+WHERE endDate < "2017-07-10 00:00:00" and status = 0; 
 
+/*+----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+
+| id | startDate           | endDate             | status | floorNumber | roomNumber | typeOfRoom | capacity | numberOfWindows | features |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+
+|  3 | 2017-07-01 15:00:00 | 2017-07-05 15:00:00 |      0 |           4 |        442 |         14 |        3 |               7 |        3 |
+|  4 | 2017-07-01 15:00:00 | 2017-07-04 15:00:00 |      0 |           8 |        855 |         14 |        2 |               4 |        1 |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+*/
 
 
 -----------------------------The list of Rooms which can be occupied by at least 3 people on a specific date
+SELECT *
+FROM rooms 
+WHERE endDate < "2017-07-10 00:00:00" and capacity >= 3; 
+
+/*+----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+
+| id | startDate           | endDate             | status | floorNumber | roomNumber | typeOfRoom | capacity | numberOfWindows | features |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+
+|  3 | 2017-07-01 15:00:00 | 2017-07-05 15:00:00 |      0 |           4 |        442 |         14 |        3 |               7 |        3 |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+*/
+
 -----------------------------The amount of unrentable Rooms (janitor closets, public laundry room, gym, etc.)
+SELECT rooms.*, tor.typeOfRoom
+FROM rooms
+INNER JOIN type_of_rooms tor ON rooms.typeOfRoom=tor.id
+WHERE rooms.typeOfRoom NOT IN (14,15,16);
+
+/*+----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+------------+
+| id | startDate           | endDate             | status | floorNumber | roomNumber | typeOfRoom | capacity | numberOfWindows | features | typeOfRoom |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+------------+
+|  7 | 0000-00-00 00:00:00 | 0000-00-00 00:00:00 |      1 |          10 |       1075 |          9 |        1 |               2 |        0 | janitor    |
+|  8 | 0000-00-00 00:00:00 | 0000-00-00 00:00:00 |      1 |           3 |        375 |          5 |        1 |               2 |        0 | gym        |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+------------+*/
+
 -----------------------------The amount of Rooms having a private Kitchen
+SELECT rooms.*, rf.feature
+FROM rooms
+INNER JOIN room_features rf ON rooms.features = rf.id
+WHERE rooms.features = 1;
+
+/*+----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+
+| id | startDate           | endDate             | status | floorNumber | roomNumber | typeOfRoom | capacity | numberOfWindows | features | feature |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+
+|  2 | 2017-07-01 15:00:00 | 2017-07-15 15:00:00 |      1 |           3 |        325 |         14 |        5 |              10 |        1 | kitchen |
+|  4 | 2017-07-01 15:00:00 | 2017-07-04 15:00:00 |      0 |           8 |        855 |         14 |        2 |               4 |        1 | kitchen |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+*/
+
 -----------------------------The average amount of windows per Floor
+SELECT floorNumber, sum(numberOfWindows)/max(floorNumber) as AVGnumberOfWindows
+FROM rooms
+GROUP BY floorNumber;
+
+/*+-------------+--------------------+
+| floorNumber | AVGnumberOfWindows |
++-------------+--------------------+
+|           3 |             4.0000 |
+|           4 |             1.7500 |
+|           5 |             1.6000 |
+|           7 |             1.1429 |
+|           8 |             0.5000 |
+|          10 |             1.0000 |
++-------------+--------------------+*/
+
+-----------------------------The amount of windows per Floor
+SELECT floorNumber, sum(numberOfWindows) as numberOfWindows
+FROM rooms
+GROUP BY floorNumber;
+
+/*+-------------+-----------------+
+| floorNumber | numberOfWindows |
++-------------+-----------------+
+|           3 |              12 |
+|           4 |               7 |
+|           5 |               8 |
+|           7 |               8 |
+|           8 |               4 |
+|          10 |              10 |
++-------------+-----------------+*/
+
 -----------------------------The amount of Floors having Rooms with carpets
+SELECT rooms.*, rf.feature
+FROM rooms
+INNER JOIN room_features rf ON rooms.features = rf.id
+WHERE rooms.features = 2;
+
+/*+----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+
+| id | startDate           | endDate             | status | floorNumber | roomNumber | typeOfRoom | capacity | numberOfWindows | features | feature |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+
+|  1 | 2017-07-01 15:00:00 | 2017-07-15 15:00:00 |      1 |           5 |        524 |         14 |        4 |               8 |        2 | carpets |
+|  5 | 2017-07-01 15:00:00 | 2017-07-15 15:00:00 |      1 |           7 |        785 |         14 |        4 |               8 |        2 | carpets |
+|  6 | 2017-07-01 15:00:00 | 2017-07-15 15:00:00 |      1 |          10 |       1024 |         14 |        4 |               8 |        2 | carpets |
++----+---------------------+---------------------+--------+-------------+------------+------------+----------+-----------------+----------+---------+*/
